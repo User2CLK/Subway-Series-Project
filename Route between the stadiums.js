0d60df3c516ab05db87eca4ef69917d80c6efb66
Charles Knowles <cknowles0826@gmail.com>
2025-03-03 19:13:35 -0500

    Add files via upload

---
+++ Route between the stadiums.js
@@ -0,0 +1,424 @@
+var data= {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Citi Field",
+          "desc": "Citi Field"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.8458213,
+            40.7570877
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Yankee Stadium",
+          "desc": "Yankee Stadium"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.9261745,
+            40.8296426
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Citi Field",
+          "desc": "The beginning of the Subway Series journey-at least form the dark side into the light.",
+          "marker-color": "#ff5908",
+          "marker-size": "large",
+          "marker-symbol": "stadium"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.8458213,
+            40.7570877
+          ]
+        },
+        "id": 2
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Yankee Stadium",
+          "desc": "The Subway Series journey is complete! Welcome to the greatest baseball stadium on Earth. ",
+          "marker-color": "#0b1f46",
+          "marker-size": "large",
+          "marker-symbol": "stadium"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.9261745,
+            40.8296426
+          ]
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mets-Willets Point Station(Citi Field to Flushing Express)",
+          "marker-color": "#ba30af",
+          "marker-size": "medium",
+          "marker-symbol": "rail-metro"
+        },
+        "geometry": {
+          "coordinates": [
+            -73.84564461157909,
+            40.75461132504796
+          ],
+          "type": "Point"
+        },
+        "id": 4
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name ": "Queensboro Plaza Station (Flushing Express to Broadway Express)",
+          "marker-color": "#93268a",
+          "marker-size": "medium",
+          "marker-symbol": "rail-metro"
+        },
+        "geometry": {
+          "coordinates": [
+            -73.94009678649326,
+            40.75054968325159
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Queensboro Plaza Station (Broadway Express to Lexington Avenue Express))",
+          "marker-color": "#fccd03",
+          "marker-size": "medium",
+          "marker-symbol": "rail-metro"
+        },
+        "geometry": {
+          "coordinates": [
+            -73.94098043698922,
+            40.750930650367366
+          ],
+          "type": "Point"
+        },
+        "id": 6
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Route of MTA service 7, known as the Flushing Express",
+          "stroke": "#ba30af",
+          "stroke-width": 3.5,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              -73.94010995181827,
+              40.75054672879537
+            ],
+            [
+              -73.93834416904957,
+              40.74955323636814
+            ],
+            [
+              -73.93332010711804,
+              40.74498694199801
+            ],
+            [
+              -73.9167558404981,
+              40.74289863931273
+            ],
+            [
+              -73.91065653499605,
+              40.74467403376167
+            ],
+            [
+              -73.90715762216377,
+              40.74530234796299
+            ],
+            [
+              -73.90481606167623,
+              40.7454918464785
+            ],
+            [
+              -73.86159410888477,
+              40.74999586414012
+            ],
+            [
+              -73.84550315746371,
+              40.754566288167126
+            ]
+          ],
+          "type": "LineString"
+        },
+        "id": 10
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MTA service 7",
+          "stroke": "#b72fac",
+          "stroke-width": 2,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              -73.8458383717628,
+              40.75456629743587
+            ],
+            [
+              -73.86050532462315,
+              40.75012276584377
+            ]
+          ],
+          "type": "LineString"
+        },
+        "id": 11
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MTA service N, known as the Broadway Express",
+          "stroke": "#fccd03",
+          "stroke-width": 3.5,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              -73.94097965908388,
+              40.75094677227017
+            ],
+            [
+              -73.94158134863032,
+              40.75129442491877
+            ],
+            [
+              -73.94176491493215,
+              40.75150301563545
+            ],
+            [
+              -73.95421602500025,
+              40.757106846294164
+            ],
+            [
+              -73.96725008370622,
+              40.76266414059927
+            ]
+          ],
+          "type": "LineString"
+        },
+        "id": 13
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Lexington Ave-59 St Station(Broadway Express to Lexington Avenue Express)",
+          "marker-color": "#e4ba03",
+          "marker-size": "medium",
+          "marker-symbol": "rail-metro"
+        },
+        "geometry": {
+          "coordinates": [
+            -73.96725871131476,
+            40.762666285525086
+          ],
+          "type": "Point"
+        },
+        "id": 14
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "59 St Station (Lexington Avenue Express to Yankee Stadium)",
+          "marker-color": "#009439",
+          "marker-size": "medium",
+          "marker-symbol": "rail-metro",
+          "iconAnchor": 50
+        },
+        "geometry": {
+          "coordinates": [
+            -73.96796877011964,
+            40.76253149876504
+          ],
+          "type": "Point"
+        },
+        "id": 15
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MTA Service 4, known as the Lexington Avenue Express",
+          "stroke": "#009439",
+          "stroke-width": 3.5,
+          "stroke-opacity": 1
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              -73.96800217469423,
+              40.76253188488181
+            ],
+            [
+              -73.9355782049858,
+              40.80687323015965
+            ],
+            [
+              -73.93469009969114,
+              40.80811039484814
+            ],
+            [
+              -73.93441473627225,
+              40.808440397546065
+            ],
+            [
+              -73.93254848550738,
+              40.81039406666736
+            ],
+            [
+              -73.93122347029141,
+              40.81179634827686
+            ],
+            [
+              -73.93074846984996,
+              40.81228952563461
+            ],
+            [
+              -73.92994164073504,
+              40.81312469323203
+            ],
+            [
+              -73.92984345174484,
+              40.81322377833294
+            ],
+            [
+              -73.92921334676143,
+              40.81397007689196
+            ],
+            [
+              -73.92904199332794,
+              40.81427119608159
+            ],
+            [
+              -73.92884235136913,
+              40.81475620650869
+            ],
+            [
+              -73.9285204369222,
+              40.81559608647646
+            ],
+            [
+              -73.92790618830006,
+              40.81723615963506
+            ],
+            [
+              -73.92679952581356,
+              40.81952304904644
+            ],
+            [
+              -73.92635186441578,
+              40.820596573573454
+            ],
+            [
+              -73.92626334914293,
+              40.82085645323323
+            ],
+            [
+              -73.9262196632712,
+              40.821026165184605
+            ],
+            [
+              -73.92619636317342,
+              40.8211540004734
+            ],
+            [
+              -73.92617597596565,
+              40.82147138338985
+            ],
+            [
+              -73.92617597695492,
+              40.82157938111871
+            ],
+            [
+              -73.92620076446043,
+              40.821853622451385
+            ],
+            [
+              -73.92622401548873,
+              40.821990270027726
+            ],
+            [
+              -73.92626193728395,
+              40.82212382609035
+            ],
+            [
+              -73.92631430101773,
+              40.82228077335043
+            ],
+            [
+              -73.92702505406798,
+              40.824382971957505
+            ],
+            [
+              -73.92711902905528,
+              40.8245661498857
+            ],
+            [
+              -73.92719612528417,
+              40.82491365161732
+            ],
+            [
+              -73.92721269985589,
+              40.82532512452127
+            ],
+            [
+              -73.92714548147929,
+              40.82562703333147
+            ],
+            [
+              -73.92697453212757,
+              40.82607091421275
+            ],
+            [
+              -73.92585102453755,
+              40.828010221318806
+            ]
+          ],
+          "type": "LineString"
+        },
+        "id": 16
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "161 St-Yankee Stadium Station(Lexington Avenue Express to Yankee Stadium)",
+          "marker-color": "#009439",
+          "marker-size": "small-medium",
+          "marker-symbol": "rail-metro"
+        },
+        "geometry": {
+          "coordinates": [
+            -73.92585117386136,
+            40.828011014661286
+          ],
+          "type": "Point"
+        },
+        "id": 17
+      }
+    ]
+  }
